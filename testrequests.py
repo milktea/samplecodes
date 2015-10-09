5fb462111aaaa0f2253cc6f9cde472117e04d22b
Milktea <milktea@Milktea.local>
2015-10-09 10:42:13 +0800

    Python requests, parsing xml, beautifulsoup

--- testrequests.py
+++ testrequests.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 result = requests.get('http://thoughtcatalog.com')
 soup = BeautifulSoup(result.text, 'html.parser')
 for item in soup.find_all('article',{'class': 'type-post'}):
-    a = item.find_all('a')[1]
+    a = item.find_all('a')[0]
     print a.attrs['href']
 
 #print result.status_code
