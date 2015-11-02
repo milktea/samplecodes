8576f9d6b50f504e738248ddb0f698bae58c0844
Milktea <milktea@Milktea.local>
2015-11-02 21:59:42 +0800

    Visualizing the data: Data Analysis with Python and Pandas

---
+++ vizualizingpanda.py
@@ -0,0 +1,27 @@
+#Visualizing the Data
+#Data Analysis with Python and Pandas
+#Nov 2, 2015 21:58
+#Milktea
+
+import pandas as pd
+import datetime
+import pandas.io.data as web
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+start = datetime.datetime(2010,1,1)
+end = datetime.datetime(2015, 7, 28)
+
+att = web.DataReader("T", "yahoo", start, end)
+
+print(att.head())
+
+att[['High', 'Low']].plot()
+plt.legend()
+plt.show()
+
+att.plot()
+plt.legend()
