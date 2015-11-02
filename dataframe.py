d954497e214a335a558dffcb5d73312829c509e4
milktea <sandrea@afterfivetech.com>
2015-11-02 22:02:38 +0800

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -5,8 +5,8 @@
 import pandas as pd
 
 starting = {'Col_1':[1,6,4,7],
-	'Col_2': [5,7,8,3],
-	'Col_2': [6,3,7,4]}
+	    'Col_2': [5,7,8,3],
+	    'Col_2': [6,3,7,4]}
 
 df = pd.DataFrame(starting)
 print(df)
